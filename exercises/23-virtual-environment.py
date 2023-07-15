ab9b5b453ee083fee3cbb3e7739588952ecf56f5
gwfernandez <gustavo.fernandez@fravega.com.ar>
2023-07-15 20:29:46 -0300

    virtual environment

---
+++ exercises/23-virtual-environment.py
@@ -0,0 +1,10 @@
+
+# > pip install virtualenv
+# > mkdir flask_project
+# > cd flask_project
+# flask_project> virtualenv venv [MAC]
+# flask_project> source venv/bin/activate [MAC]
+# flask_project> pip install Flask
+# flask_project> pip freeze
+# flask_project> deactivate
+
